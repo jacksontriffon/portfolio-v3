89e1acb1874fe7a191b961b11c04e7d7bbcc01ab
jacksontriffon <jackson.triffon@gmail.com>
2025-06-09 13:18:42 +1000

    📈 analytics: sign in event

--- src/app/_components/auth/AuthButton.tsx
+++ src/app/_components/auth/AuthButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import posthog from "posthog-js";
 import { useTransition } from "react";
 import { toggleAuth } from "~/app/_actions/authActions";
 
@@ -9,6 +10,9 @@ export function AuthButton({ isSignedIn }: { isSignedIn: boolean }) {
   return (
     <form action={toggleAuth} className="w-fit">
       <button
+        onClick={() => {
+          posthog.capture("sign-in-clicked", { isSignedIn });
+        }}
         type="submit"
         disabled={isPending}
         className="border-antique-500 text-antique-900 hover:outline-antique-900 cursor-pointer rounded-sm border px-6 py-2 text-sm font-semibold no-underline outline outline-offset-2 outline-transparent transition"
